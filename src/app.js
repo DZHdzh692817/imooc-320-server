f6db25257530bdf193b4074455212af44ca15f4b
DZHdzh692817 <1120951042@qq.com>
2023-11-02 22:33:30 +0800

    工作流自动发布到线上服务器

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ app.use(router.routes(), router.allowedMethods())
 
 // error-handling
 app.on('error', (err, ctx) => {
-    console.error('server error', err, ctx)
+    console.error('server ook error', err, ctx)
 });
 
 module.exports = app
